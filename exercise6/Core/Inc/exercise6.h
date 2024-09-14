769611caeb783e8c8ae6c3a24ff6ec0a7647d5ed
BROOKYNE <pnguyen196196@gmail.com>
2024-09-14 21:21:21 +0700

    add ex6

---
+++ exercise6/Core/Inc/exercise6.h
@@ -0,0 +1,17 @@
+/*
+ * exercise6.h
+ *
+ *  Created on: Sep 13, 2024
+ *      Author: USER
+ */
+
+#ifndef INC_EXERCISE6_H_
+#define INC_EXERCISE6_H_
+
+#include "main.h"
+
+void init_exercise6();
+void exercise6_run();
+void ClockSimulation(int num);
+
+#endif /* INC_EXERCISE6_H_ */
