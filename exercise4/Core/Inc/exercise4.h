6cbd60cc7f404c4fea48c113e036aab4cd73def2
BROOKYNE <pnguyen196196@gmail.com>
2024-09-14 21:13:35 +0700

    add ex4

---
+++ exercise4/Core/Inc/exercise4.h
@@ -0,0 +1,17 @@
+/*
+ * exercise4.h
+ *
+ *  Created on: Sep 13, 2024
+ *      Author: USER
+ */
+
+#ifndef INC_EXERCISE4_H_
+#define INC_EXERCISE4_H_
+
+#include "main.h"
+
+void init_exercise4();
+void exercise4_run();
+void display7SEG(int num);
+
+#endif /* INC_EXERCISE4_H_ */
