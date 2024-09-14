1a1a042af6620ed3d10cbe82d86048f7014aed1c
BROOKYNE <pnguyen196196@gmail.com>
2024-09-14 21:09:38 +0700

    add ex2

---
+++ exercise2/Core/Inc/exercise2.h
@@ -0,0 +1,18 @@
+/*
+ * exercise2.h
+ *
+ *  Created on: Sep 13, 2024
+ *      Author: USER
+ */
+
+#ifndef INC_EXERCISE2_H_
+#define INC_EXERCISE2_H_
+
+#include "main.h"
+typedef enum {
+	red_state, yellow_state, green_state,
+} state;
+void init_exercise2();
+void exercise2_run();
+
+#endif /* INC_EXERCISE2_H_ */
