39dac06471c22ab408a25a59fab73522aea69a69
BROOKYNE <pnguyen196196@gmail.com>
2024-09-14 21:24:10 +0700

    add ex8

---
+++ exercise8/Core/Inc/exercise8.h
@@ -0,0 +1,17 @@
+/*
+ * exercise8.h
+ *
+ *  Created on: Sep 13, 2024
+ *      Author: USER
+ */
+
+#ifndef INC_EXERCISE8_H_
+#define INC_EXERCISE8_H_
+
+#include"main.h"
+
+void init_exercise8();
+void exercise8_run();
+void setNumberOnClock();
+
+#endif /* INC_EXERCISE8_H_ */
