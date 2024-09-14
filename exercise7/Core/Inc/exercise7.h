391b2311b48ad45eac71dd1e53ebc1f3855a8ee6
BROOKYNE <pnguyen196196@gmail.com>
2024-09-14 21:22:54 +0700

    add ex7

---
+++ exercise7/Core/Inc/exercise7.h
@@ -0,0 +1,17 @@
+/*
+ * exercise7.h
+ *
+ *  Created on: Sep 13, 2024
+ *      Author: USER
+ */
+
+#ifndef INC_EXERCISE7_H_
+#define INC_EXERCISE7_H_
+
+#include "main.h"
+
+void init_exercise7();
+void exercise7_run();
+void clearAllClock();
+
+#endif /* INC_EXERCISE7_H_ */
